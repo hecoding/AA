9aa213322c84f9ab7fba9f13d7c7e8e84c6086c9
hecoding <hlaria@ucm.es>
2015-11-03 06:18:08 +0100

    principio segunda parte

---
+++ practica2/src/costRegulariz.m
@@ -0,0 +1,8 @@
+function [J, grad] = cost(theta, X, y, lambda)
+% theta is a row vector
+  m = length(y);
+  
+  %J = ;
+  %grad = ;
+
+end
---
+++ practica2/src/dos.m
@@ -0,0 +1,29 @@
+data = load('ex2data2.txt');
+y = data(:, 3);
+
+% Obtain vectors with negative and positive example indices
+neg = find (y == 0);
+pos = find (y == 1);
+
+% Draw examples
+hold on;
+plot(data(pos, 1), data(pos, 2), 'k+', 'MarkerSize', 7);
+plot(data(neg, 1), data(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+xlabel('Microchip Test 1', 'fontsize', 10);
+ylabel('Microchip Test 2', 'fontsize', 10);
+legend('Pass', 'Fail');
+legend('boxoff');
+
+X = mapFeature(data(:,1), data(:,2));
+
+
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+% Obtain the optimal theta
+lambda = 1;
+[theta, cst] = fminunc(@(t)(costRegulariz(t, X, y, lambda)), [0 0 0] , options);
+plotDecisionBoundary(theta, X, y);
+
+% falta costRegulariz
+% no se si ese vector de lambdas es corto o no
+% jugar con varias lambdas
